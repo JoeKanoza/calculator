4bbdc87b667a672127c333110c774fdd8a5c584c
Joe Kanoza <joe@joekanoza.com>
2022-01-26 10:00:10 -0500

    Reorganized and cleaned up

---
+++ include/token_stream.h
@@ -0,0 +1,99 @@
+/*
+ * ===========================================================================
+ *
+ *       Filename:  token_stream.h
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  Wed Jan 26 2022
+ *       Revision:
+ *       Compiler:
+ *
+ *         Author:  Joe Kanoza
+ *   Organization:  ${ORG}
+ *
+ * ===========================================================================
+ */
+
+#pragma once
+
+class Token_stream {
+	public:
+		Token_stream();
+		Token get();
+		void putback(Token t);
+		void ignore(char c);
+
+	private:
+		bool full {false};		// is there a Token in the buffer?
+		Token buffer {0};		//putback() save uts token here
+};
+
+Token_stream::Token_stream()
+	:full(false), buffer(0)
+{
+}
+
+Token Token_stream::get()
+{
+	if(full)
+	{
+		full = false;
+		return buffer;
+	}
+    char ch;
+    cin>>ch;
+    switch(ch){
+        case print:
+        case quit:
+        case '(':
+		case ')':
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '{':
+		case '}':
+		case '!':
+		case '%':
+            return Token{ch};
+        case '.':
+        case '0': case '1': case '2': case '3': case '4': case '5':
+        case '6': case '7': case '8': case '9':
+        {
+            cin.putback(ch);
+            double val;
+            cin>>val;
+            return Token{number,val};
+        }
+        default:
+            error("Bad token");
+    }
+}
+
+void Token_stream::putback(Token t)
+{
+	if(full) error("putback() into a full buffer");
+	buffer = t;
+	full = true;
+}
+
+void Token_stream::ignore(char c)
+// c represents the kind of token.
+{
+	// look first in buffer
+	if(full && c==buffer.kind)
+	{
+		full = false;
+		return;
+	}
+	full = false;
+
+	// search for input
+	char ch = 0;
+	while(cin>>ch)
+		if(ch==c)return;
+}
+
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include <token.h>
+#include "token.h"
+#include "token_stream.h"
 
 // Token getToken();
 
-// Token_stream ts = {};
+Token_stream ts = {};
 
 int main(int, char**) {
     try
@@ -23,5 +24,151 @@ int main(int, char**) {
         keep_window_open("~~");
         return 2;
     }
+}
+
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind)
+	{
+	case '{':
+		{
+			double d = expression();
+			t = ts.get();
+			if(t.kind != '}') error("'}' expected");
+			return d;
+		}
+	case '(':
+		{
+			double d = expression();
+			t = ts.get();
+			if(t.kind != ')') error("')' expected");
+			return d;
+		}
+	case number:		// we use '8' to represent a number
+		{
+			double d = t.value;
+			t = ts.get();
+
+			if(t.kind == '!')
+			{
+				int sum = narrow_cast<int>(d);
+				if(sum==0) return 1;
+				for(int i = sum-1; i > 0; --i)
+				{
+					sum *= i;
+				}
+				return sum;
+			}
+			else
+			{
+				ts.putback(t);
+				return d;		// return the number's value
+			}
+		}
+	case '-':
+		return - primary();
+	case '+':
+		return primary();
+
+	default:
+		error("primary expected");
+	}
+}
+
+// double factorial()
+// {
+// 	double left = primary();
+// }
+
+double term()
+{
+	double left = primary();
+	Token t = ts.get(); 			// get the next Token from the Token stream
+
+	while (true)
+	{
+		switch(t.kind)
+		{
+			case '*':
+				left *= primary();
+				t = ts.get();
+				break;
+
+			case '/':
+				{
+					double d = primary();
+					if(d == 0) error("divide by zero");
+					left /= d;
+					t = ts.get();
+					break;
+				}
+
+			case '%':
+				{
+					double d = primary();
+					if(d ==0) error("% divided by zero");
+					left = fmod(left, d);
+					t=ts.get();
+					break;
+				}
+			default:
+				ts.putback(t);
+				return left;
+		}
+	}
+}
+
+double expression()
+{
+	double left = term();	// read and evaluate a Term
+	Token t = ts.get();		// get the next Token from the Token stream
+
+	while (true)
+	{
+		switch (t.kind)
+		{
+		case '+':
+			left += term();		// evaluate Term and add
+			t = ts.get();
+			break;
+
+		case '-':
+			left-= term();		// evaluate Term and subtract
+			t = ts.get();
+			break;
+
+		default:
+			ts.putback(t); 	// put t back into the token stream
+			return left;	// finally: no more + or -; return the answer
+		}
+	}
+}
+
+void calculate()
+{
+	while(cin)
+		try
+		{
+			cout << prompt;
+			Token t = ts.get();
+			while(t.kind == print) t=ts.get();        // eat ';'
+			if(t.kind == quit) {
+				// keep_window_open();
+				return;
+			}
+			ts.putback(t);
+			cout<<result<<expression()<<'\n';
+		}
+		catch(exception& e)
+		{
+			cerr<<e.what()<<'\n';
+			clean_up_mess();
+		}
+}
+
+void clean_up_mess()
+{
+	ts.ignore(print);
 }
 
